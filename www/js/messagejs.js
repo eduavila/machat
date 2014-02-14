5a53ba9b9e7dd7f9cb3294f2f0fc8eebbbc71297
Eduardo Avila Malheiros <edu.avila2@gmail.com>
2014-02-14 17:00:43 -0400

    Imprementado lista com swipe.

---
+++ www/js/messagejs.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/* arquivando msg com swiperinght*/
+
+
+$(document).on("pagecreate","#demo-page",function(){
+    // Swipe para remove da lista
+    $(document).on("swipeleft swiperinght","#list li",function(event)){
+        var listitem=$this,
+    }
+});
